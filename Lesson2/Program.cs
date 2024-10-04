21ecd4f1d7c96dcf339e2f53ddd71b3dc4d00cca
carantoin <90127329+carantoin@users.noreply.github.com>
2024-10-04 14:57:19 +0200

    Add files via upload

---
+++ Lesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿try
+{
+    Console.Write("Введите a:");
+    double a = double.Parse(Console.ReadLine());
+    Console.Write("Введите b:");
+    double b = double.Parse(Console.ReadLine());
+    double x = (a + b) * (a + b);
+    double y = a * a + b * b;
+    if (x > y)
+    {
+        Console.WriteLine("Квадрат суммы больше");
+    }
+    else if (x < y)
+
+    {
+        Console.WriteLine("Сумма квадратов больше");
+    }
+}
+catch
+{
+    Console.WriteLine("Введите правильные данные");
+}
